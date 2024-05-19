67aff26af9562d8ec40599586744d83de5eeff17
KThurmes <thurmesk@oregonstate.edu>
2024-05-19 01:27:42 -0600

    Add entry implementation

--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,22 @@ import mysql from "mysql2/promise";
 import path from "path";
 import { engine } from 'express-handlebars';
 
+
 import https from 'https';
 import fs from 'fs';
 
-const pages = ['actions', 'locations', 'plants', 'sensor_readings', 'sensors', 'updates'];
+
+import {getColumnHeaderDictionary} from './stringDictionary.js';
+import {GetInsertQueryString} from './SQLQueries.js';
+
+
+const pages = ['actions', 'locations', 'plants', 'sensor_readings', 'sensors', 'updates', 'light_categories', 'action_types'];
 const testData = {};
+
 
 async function main() {
     console.log("Starting backend!");
+
 
     const connection = await mysql.createConnection({
         host: process.env.DB_HOST,
@@ -19,6 +27,7 @@ async function main() {
         database: process.env.DB_NAME,
     });
 
+
     // This just uses the stuff we got once
     // In the future, we will need to fetch dynamically with every request
     for (const page of pages) {
@@ -26,6 +35,7 @@ async function main() {
         const [schemaResult] = await connection.execute(`DESCRIBE ${page}`);
         // Build schema as object (for help with forms)
         const schemaFields = Object.fromEntries(schemaResult.map(row => [row.Field, null]));
+
 
         // Get FK relationships from table
             // Yields eg
@@ -46,6 +56,7 @@ async function main() {
             AND REFERENCED_TABLE_NAME IS NOT NULL
             `, [page]);
 
+
         // Fill FK fields with names for CRUD menus
         // TODO also do this with ENUMs
         for (const fk of fkResult) {
@@ -54,8 +65,10 @@ async function main() {
             schemaFields[fk.COLUMN_NAME] = names.map(row => row.NAME);
         }
 
+
         // Construct the base query
         let baseQuery = `SELECT ${page}.*`;
+
 
         // Construct JOIN clauses for foreign keys
         let joinClauses = '';
@@ -66,21 +79,29 @@ async function main() {
         // Complete the query
         const fullQuery = `${baseQuery} FROM ${page}${joinClauses}`;
 
+
         // Fetch table data with the constructed query
         const [dataResult] = await connection.execute(fullQuery);
         testData[page] = [schemaFields, ...dataResult];
     }
 
+
     const app = express();
     app.engine('handlebars', engine({ defaultLayout: 'main' }));
     app.set('view engine', 'handlebars');
 
+
     app.use(express.static('frontend'));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: false }))
+    //app.use(express.static("public"))
+
 
     pages.forEach(page => {
         app.get(`/${page}`, (req, res) => {
             // Render the corresponding Handlebars template
-            console.log(testData[page])
+
+
             res.render('crud', {
                 title: `${page.charAt(0).toUpperCase() + page.slice(1)} Page`,
                 pageName: page,
@@ -88,7 +109,24 @@ async function main() {
                 entries: testData[page]
             });
         });
-    });
+        app.post(`/${page}/create`, async (req, res) =>
+            {
+                const obj = JSON.parse(JSON.stringify(req.body));
+               
+                if(req.body !== null)
+                {
+                    const queryString = GetInsertQueryString(page, obj);
+                    await connection.execute(queryString);
+                    //Todo: must change response to update the table
+                    res.redirect('/');
+                }
+                else{
+                    res.status(401).send("Please check your stuff.");
+                }
+            }
+            );
+        });
+
 
     // Define routes
     app.get('/', (req, res) => {
@@ -98,6 +136,7 @@ async function main() {
         });
     });
 
+
     if (process.env.IS_PROD === "true") {
         if (typeof process.env.CERT_PATH === 'undefined') {
             throw new Error('CERT_PATH environment variable is not set.');
@@ -105,9 +144,12 @@ async function main() {
         const privateKey = fs.readFileSync(path.join(process.env.CERT_PATH, "privkey.pem"), "utf8");
         const certificate = fs.readFileSync(path.join(process.env.CERT_PATH, "fullchain.pem"), "utf8");
 
+
         const credentials = { key: privateKey, cert: certificate };
 
+
         const httpsServer = https.createServer(credentials, app);
+
 
         httpsServer.listen(443, () => {
             console.log('HTTPS server running on port 443');
@@ -122,5 +164,6 @@ async function main() {
         });
     }
 }
+
 
 main().catch(console.error);
